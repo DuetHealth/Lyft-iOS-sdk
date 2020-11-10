b6ff92f4b6bce98a358bef5017ed092a1a40ab36
KZ <me@kez.io>
2020-11-10 06:32:43 -0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "LyftSDK",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macOS(.v10_15),
+    ],
+    products: [
+        .library(name: "LyftSDK", targets: ["LyftSDK"])
+    ],
+    targets: [
+        .target(name: "LyftSDK", dependencies: [])
+    ]
+)

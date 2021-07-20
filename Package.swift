6ce1592c1e05360ddbe44908e1e74141fab67be8
Kesi Maduka <kesi.maduka@duethealth.com>
2021-07-20 00:44:58 -0400

    Undo this.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,9 @@ import PackageDescription
 let package = Package(
     name: "LyftSDK",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macOS(.v10_15),
     ],
     products: [
         .library(name: "LyftSDK", targets: ["LyftSDK"])
